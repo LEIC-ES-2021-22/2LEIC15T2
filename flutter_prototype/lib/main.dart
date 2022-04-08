9db4e45370aac70d49ec000c3403b485315bed6e
Miggs7 <62034549+Miggs7@users.noreply.github.com>
2022-04-08 20:45:15 +0100

    Update main.dart

--- flutter_prototype/lib/main.dart
+++ flutter_prototype/lib/main.dart
@@ -6,7 +6,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'FEUQ',
+      title: 'FEUPQ',
       theme: ThemeData(primarySwatch: Colors.deepOrange),
       home: Home(),
     );
